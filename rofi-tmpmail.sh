dde1677154affbb13a44e1239f0a02192c8ba641
dadav <33197631+dadav@users.noreply.github.com>
2020-11-01 15:24:49 +0100

    add i3 support

--- rofi-tmpmail.sh
+++ rofi-tmpmail.sh
@@ -2,7 +2,7 @@
 
 # Select an action
 if [[ $# -eq 0 ]]; then
-  action="$(echo -e 'copy\ngenerate\nlist\nrecent' | rofi -dmenu -p 'action')"
+  action="$(echo -e 'copy\ngenerate\nlist\nrecent' | rofi -show drun -dmenu -p 'action')"
   exec "$0" "$action"
 fi
 
@@ -10,6 +10,17 @@ function notify() {
   if command -v notify-send >/dev/null 2>&1; then
     notify-send -u normal "$1"
   fi
+}
+
+function term() {
+  t="xterm"
+  for t in kitty urxvt termine xterm; do
+    if command -v $t >/dev/null 2>&1; then
+      exec $t -e $*
+    fi
+  done
+
+  notify "Terminal not supported"
 }
 
 action="$1"
@@ -27,9 +38,9 @@ case "$action" in
   list)
     mails="$(tmpmail | grep '^[0-9]')"
     mail_to_show="$(echo "$mails" | rofi -dmenu -p 'open' | awk '{print $1}')"
-    tmpmail "$mail_to_show"
+    term tmpmail "$mail_to_show"
     ;;
   recent)
-    tmpmail -r
+    term tmpmail -r
     ;;
 esac
