fc1000d7288f941ed9a3d9fa32004d21f4ab916d
dadav <33197631+dadav@users.noreply.github.com>
2020-11-01 14:47:54 +0100

    added code

--- rofi-tmpmail.sh
+++ rofi-tmpmail.sh
@@ -2,20 +2,34 @@
 
 # Select an action
 if [[ $# -eq 0 ]]; then
-  action="$(echo -e 'generate\nlist\nrecent' | rofi -dmenu -p 'action')"
+  action="$(echo -e 'copy\ngenerate\nlist\nrecent' | rofi -dmenu -p 'action')"
   exec "$0" "$action"
 fi
 
-action="$1"
+function notify() {
+  if command -v notify-send >/dev/null 2>&1; then
+    notify-send -u normal "$1"
+  fi
+}
 
+action="$1"
 case "$action" in
+  copy)
+    mail="$(tmpmail | awk 'NR==1{ print $4 }')"
+    echo -n "$mail" | xclip -i
+    notify "Copied $mail to clipboard"
+    ;;
   generate)
-    echo "generate"
+    mail="$(tmpmail -g)"
+    echo -n "$mail" | xclip -i
+    notify "Copied $mail to clipboard"
     ;;
   list)
-    echo "list"
+    mails="$(tmpmail | grep '^[0-9]')"
+    mail_to_show="$(echo "$mails" | rofi -dmenu -p 'open' | awk '{print $1}')"
+    tmpmail "$mail_to_show"
     ;;
   recent)
-    echo "recent"
+    tmpmail -r
     ;;
 esac
